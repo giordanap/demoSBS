3e92547fd7da3696a6e2e1e0d2faa86e12bfa74e
Giordan Arredondo <garredondop@uni.pe>
2022-06-27 13:20:32 -0500

    configuration init

---
+++ src/main/java/com/examen/demoSBS/DemoSbsApplication.java
@@ -0,0 +1,13 @@
+package com.examen.demoSBS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSbsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSbsApplication.class, args);
+	}
+
+}
